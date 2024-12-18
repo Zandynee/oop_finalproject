67122374f32edfaa80256ef8f9cd0a75b331b551
Zandynee <mulat625@gmail.com>
2024-12-18 08:57:08 +0700

    nyoba main menu

---
+++ oop_geming/MenuButton.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class MenuButton : Godot.MenuButton
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		var button = new Button();
+   	button.Text = "Click me";
+	button.Pressed += ButtonPressed;
+	AddChild(button);
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+	private void ButtonPressed(){
+	GD.Print("heyyy");}
+}
