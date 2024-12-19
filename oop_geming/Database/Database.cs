ce6da72ca7d3a6bc11fa71480df0435034ebfb29
SentientCorn <radityanarotama0@gmail.com>
2024-12-19 14:52:52 +0700

    added some database command for character (PLACEHOLDER FOR NOW)

--- oop_geming/Database/Database.cs
+++ oop_geming/Database/Database.cs
@@ -1,5 +1,7 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public partial class Database : Control
 {
@@ -14,6 +16,34 @@ public partial class Database : Control
 	{
 	}
 
+	// Backend Character
+	  public void addCharacter(Character character){
+    context.Characters.Add(character);
+    context.SaveChanges();
+  }
+
+  public void updateCharacter(Character character){
+    context.Characters.Update(character);
+    context.SaveChanges();
+  }
+
+  public void deleteCharacter(Character character){
+    context.Characters.Remove(character);
+    context.SaveChanges();
+  }
+
+  public Character getCharacter(int id){
+    return context.Characters.Find(id);
+  }
+
+  public Character getCharacter(string name){
+		var character = context.Characters.Where(c => c.Name == name).FirstOrDefault();
+    return context.Characters.Where(c => c.Name == name).FirstOrDefault();
+  }
+
+  public List<Character> getCharacters(){
+    return context.Characters.ToList();
+  }
 
 
 }
