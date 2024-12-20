230dc926fac8a3dc2d6e1ee23155cebc1bc9fc92
SentientCorn <radityanarotama0@gmail.com>
2024-12-20 11:31:21 +0700

    Fixed fight, somehow broke it

--- oop_geming/map/fight.cs
+++ oop_geming/map/fight.cs
@@ -6,22 +6,7 @@ public partial class fight : Button
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-			// Access the GameState singleton
-		Node parentNode = GetParent();
-		var gameState = parentNode.GetNode<GameState>("GameState");
-		// Retrieve the rooms and process them
-		if (gameState.Rooms.Count == 0)
-		{
-			GD.Print("No rooms found in GameState.");
-			return;
-		}
-
-		GD.Print("Processing received rooms:");
-		foreach (var room in gameState.Rooms)
-		{
-			GD.Print($"Room Type: {room.RoomType}");
-		}
-	 
+		
 	this.Pressed += FightPressed;
 	
 	
--- oop_geming/ui_battle/ui_battle.cs
+++ oop_geming/ui_battle/ui_battle.cs
@@ -2,7 +2,7 @@
 using Godot;
 using System;
 
-public partial class battleui : Node
+public partial class ui_battle : Node
 {
 	
 	public override void _Ready()
